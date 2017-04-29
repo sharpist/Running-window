e0645106f152e3f5af4c80706d84baf6a2a9841e
Alexander Usov <alexusov@photographer.net>
2017-04-29 16:46:09 +0300

    Running-window
    little joke program

---
+++ Убегающее окно/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace Убегающее_окно
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.помощьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.описаниеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.Green;
+            this.label1.Location = new System.Drawing.Point(12, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(368, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Вы стремитесь сделать мир лучше?";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.Color.Green;
+            this.button1.Location = new System.Drawing.Point(12, 116);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 34);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Да, конечно!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.ForeColor = System.Drawing.Color.Red;
+            this.button2.Location = new System.Drawing.Point(263, 116);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(109, 34);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Нет";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.Transparent;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.помощьToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(384, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // помощьToolStripMenuItem
+            // 
+            this.помощьToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.описаниеToolStripMenuItem,
+            this.выходToolStripMenuItem});
+            this.помощьToolStripMenuItem.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.помощьToolStripMenuItem.Name = "помощьToolStripMenuItem";
+            this.помощьToolStripMenuItem.Size = new System.Drawing.Size(68, 20);
+            this.помощьToolStripMenuItem.Text = "Помощь";
+            // 
+            // описаниеToolStripMenuItem
+            // 
+            this.описаниеToolStripMenuItem.BackColor = System.Drawing.Color.Transparent;
+            this.описаниеToolStripMenuItem.Name = "описаниеToolStripMenuItem";
+            this.описаниеToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.описаниеToolStripMenuItem.Text = "Описание";
+            this.описаниеToolStripMenuItem.Click += new System.EventHandler(this.описаниеToolStripMenuItem_Click);
+            // 
+            // выходToolStripMenuItem
+            // 
+            this.выходToolStripMenuItem.BackColor = System.Drawing.Color.Transparent;
+            this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            this.выходToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.выходToolStripMenuItem.Text = "Выход";
+            this.выходToolStripMenuItem.Click += new System.EventHandler(this.выходToolStripMenuItem_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.textBox1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.textBox1.Location = new System.Drawing.Point(16, 52);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(32, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.textBox2.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.textBox2.Location = new System.Drawing.Point(16, 78);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(32, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(384, 162);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximumSize = new System.Drawing.Size(400, 200);
+            this.MinimumSize = new System.Drawing.Size(400, 200);
+            this.Name = "Form1";
+            this.Text = "Убегающее окно";
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem помощьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem описаниеToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ Убегающее окно/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Убегающее_окно
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void описаниеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Добро пожаловать в забавное приложение «Убегающее окно». Постарайтесь ответить верно, особенно усердные смогут попасть по кнопке «Да, конечно!».\r\n     PS: сделал 12 янв. 2017 на C#, Александр Усов", "Описание", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void выходToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            // Переводит координату X в строку и записывает в поля ввода
+            textBox1.Text = e.X.ToString();
+            // Переводит координату Y в строку и записывает в поля ввода
+            textBox2.Text = e.Y.ToString();
+            // Мы объявили экземпляр класса Random (rnd), с помощью которого будем генерировать случайные числа
+            // Используем код, вида rnd.Next (диапазон_генерации) или rnd.Next (от, до -1)
+            Random rnd = new Random();
+            // Также объявим еще несколько переменных
+            // tmp_location объявляется, чтобы временно хранить положение окна
+            // А _h и _w будут хранить в себе размеры экрана, чтобы при случайном перемещении окно не вышло за его пределы
+            Point tmp_location;
+            int _w = System.Windows.Forms.SystemInformation.PrimaryMonitorSize.Width;
+            int _h = System.Windows.Forms.SystemInformation.PrimaryMonitorSize.Height;
+            // Если координата по оси X и по оси Y лежит в очерчиваемом вокруг кнопки "Да, конечно!" квадрате
+            if (e.X > 0 && e.X < 130 && e.Y > 100 && e.Y < 160)
+            {
+                // Запоминаем текущее положение окна
+                tmp_location = this.Location;
+                // Генерируем перемещения по осям X и Y и прибавляем их к хранимому значению текущего положения окна
+                // Числа генерируются в диапазоне от -100 до 100.
+                tmp_location.X += rnd.Next(-100, 100);
+                tmp_location.Y += rnd.Next(-100, 100);
+                // Если окно вылезло за пределы экрана по одной из осей
+                if (tmp_location.X < 0 || tmp_location.X > (_w - this.Width / 2) || tmp_location.Y < 0 || tmp_location.Y > (_h - this.Height / 2))
+                {
+                    // Новыми координатами станет центр окна
+                    tmp_location.X = _w / 2;
+                    tmp_location.Y = _h / 2;
+                }
+                // Обновляем положение окна, на новое сгенерированное
+                this.Location = tmp_location;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Вывести сообщение с текстом "Вы усердны!"
+            // Второй параметр - заголовок окна сообщения "Результат"
+            // MessageBoxButtons.OK - тип размещаемой кнопки на форме сообщения
+            // MessageBoxIcon.Information - тип сообщения - будет иметь иконку "информация" и соответствующий звуковой сигнал
+            MessageBox.Show("Вы усердны!", "Результат", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            // Завершить приложение
+            Application.Exit();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Вывести сообщение с текстом "Мы не сомневались в вешем безразличии..."
+            MessageBox.Show("Мы не сомневались в вешем безразличии...", "Результат", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
